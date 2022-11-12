cf42ca5a80ef690b7026d38c8719ebb800b8e7f8
ilpadrello <app.panebianco@gmail.com>
2022-11-12 12:40:56 +0100

    add JSDoc example for integration with JS

---
+++ typescript/dist/modules/test.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calculateTax = void 0;
+/**
+ *
+ * @param { number } value
+ * @returns { number }
+ */
+function calculateTax(value) {
+    return value * 0.3;
+}
+exports.calculateTax = calculateTax;
---
+++ typescript/ts/js-modules/test.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param { number } value
+ * @returns { number }
+ */
+export function calculateTax(value) {
+  return value * 0.3;
+}
